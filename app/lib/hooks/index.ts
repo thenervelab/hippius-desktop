4fd50b1e3f2bbc428b3310d25c56aebb19c72ce9
raoarhamfaisal <arhamfaisal780@gmail.com>
2025-07-15 11:53:56 +0500

    removed unused hook

--- app/lib/hooks/index.ts
+++ app/lib/hooks/index.ts
@@ -3,4 +3,3 @@ export { default as useAbstractCity } from "./use-abstract-city";
 export { default as useFps } from "./use-fps";
 export { default as useBreakpoint } from "./use-breakpoint";
 export { default as usePagination } from "./use-pagination";
-export { default as useFocusPoint } from "./useFocusPoint";
