943a2aa411b9ce199c845096a4491c5638db187f
raoarhamfaisal <arhamfaisal780@gmail.com>
2025-08-07 16:36:15 +0500

    remove scale factor

--- app/lib/hooks/useWindowSizer.ts
+++ app/lib/hooks/useWindowSizer.ts
@@ -18,11 +18,10 @@ export function useWindowSizer() {
       if (!mon) return;
 
       const phys = mon.size;
-      const uiScale = mon.scaleFactor;
       const rawW = phys.width > 1470 ? 1320 : phys.width * 0.9;
       const rawH = phys.height * 0.9;
-      const finalW = Math.round(rawW / uiScale);
-      const finalH = Math.round(rawH / uiScale);
+      const finalW = Math.round(rawW);
+      const finalH = Math.round(rawH);
 
       await win.setSize(new PhysicalSize(finalW, finalH));
       await win.show();
