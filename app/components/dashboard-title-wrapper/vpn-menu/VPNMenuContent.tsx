f1fa2e8e6acd1fec884dba4038237dc196b118b7
raoarhamfaisal <arhamfaisal780@gmail.com>
2025-11-28 15:21:25 +0500

    Remove build errors

--- app/components/dashboard-title-wrapper/vpn-menu/VPNMenuContent.tsx
+++ app/components/dashboard-title-wrapper/vpn-menu/VPNMenuContent.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React from "react";
 import Image from "next/image";
 import { motion, AnimatePresence } from "framer-motion";
 import { useAtom } from "jotai";
