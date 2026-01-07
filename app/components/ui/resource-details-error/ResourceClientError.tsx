8ee4972c65ecf3f0b09b94e371ba765a26998df3
raoarhamfaisal <arhamfaisal780@gmail.com>
2026-01-08 01:43:26 +0500

    Remove unused components, constants, hooks, and utility functions to streamline the codebase

--- app/components/ui/resource-details-error/ResourceClientError.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { ResourceDetailsError } from "@/app/components/ui";
-import { ResourceDetailsErrorProps } from ".";
-
-export default function ResourceClientError(props: ResourceDetailsErrorProps) {
-  return <ResourceDetailsError {...props} />;
-}
--- app/lib/helpers/mnemonic.ts
+++
@@ -1,6 +0,0 @@
-import { cryptoWaitReady, mnemonicGenerate } from "@polkadot/util-crypto";
-
-export async function generateMnemonic(): Promise<string> {
-  await cryptoWaitReady();
-  return mnemonicGenerate();
-}
