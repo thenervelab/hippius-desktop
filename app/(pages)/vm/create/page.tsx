f6992ac7a5813b752f6ac1a2b7d73f9614fe8989
raoarhamfaisal <arhamfaisal780@gmail.com>
2026-01-19 12:57:57 +0500

    Add subText to DashboardTitleWrapper in VM pages for consistency

--- app/(pages)/vm/create/page.tsx
+++ app/(pages)/vm/create/page.tsx
@@ -3,7 +3,10 @@ import CreateVM from "@/components/vm/create-vm";
 
 export default function CreateVMPage() {
   return (
-    <DashboardTitleWrapper mainText="Virtual Machines">
+    <DashboardTitleWrapper
+      mainText="Virtual Machines"
+      subText="All virtual machines run in isolated enclaves with hardware-level encryption"
+    >
       <div className="mt-6">
         <CreateVM />
       </div>
