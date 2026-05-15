eac244c120c72811f8483c437baba77f063e6b44
ahmadrao <ahmadraosanawarali@gmail.com>
2026-05-15 12:43:54 +0500

    feat(updater): add Decoration icon and laptop illustrations
    Decoration paints the brand-blue grid revealed inside an elliptical
    mask behind the Hippius logo on the new update dialog. Laptop PNGs
    ship light/dark variants that swap via the `dark:` class toggle.

--- app/components/ui/icons/index.ts
+++ app/components/ui/icons/index.ts
@@ -161,6 +161,7 @@ export { default as Note2 } from "./Note2";
 export { default as Edit } from "./Edit";
 export { default as Repeat } from "./Repeat";
 export { default as CloseSquare } from "./CloseSquare";
+export { default as Decoration } from "./Decoration";
 export { default as Ticket } from "./Ticket";
 export { default as Building } from "./Building";
 export { default as CircularTickGrid } from "./CircularTickGrid";
